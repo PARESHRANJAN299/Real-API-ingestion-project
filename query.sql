2ee073b257c0578fe7f387c999e4ff4062e05d91
Paresh Ranjan Rout <104611685+PARESHRANJAN299@users.noreply.github.com>
2025-07-12 16:46:21 +0530

    Create schema in the sql db for soruce

---
+++ query.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Products (
+  id INT,
+  title NVARCHAR(255),
+  price FLOAT,
+  description NVARCHAR(MAX),
+  category NVARCHAR(100),
+  image NVARCHAR(500),
+  rating_rate FLOAT,
+  rating_count INT
+);
